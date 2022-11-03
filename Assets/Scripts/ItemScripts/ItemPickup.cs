07011ca9f15def2f772db4320d913495caa44179
Ingrid O'Connor <ingridoco@gmail.com>
2022-11-03 11:40:00 -0500

    changed dialogue trigger

--- Assets/Scripts/ItemScripts/ItemPickup.cs
+++ Assets/Scripts/ItemScripts/ItemPickup.cs
@@ -6,10 +6,16 @@ public class ItemPickup : MonoBehaviour
 {
     public Item item;
 
+    public DialogueTree dialogue;
+    public DialogueManager dialogueManager;
+
     void Pickup()
     {
         InventoryManager.instance.Add(item);
         Destroy(gameObject);
+
+        // trigger dialogue
+        dialogueManager.StartDialogue(dialogue);
     }
 
     private void OnMouseDown()
