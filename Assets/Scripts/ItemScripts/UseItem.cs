47c67d62150891319c3c28b55c0a83bfee088d94
Ingrid O'Connor <ingridoco@gmail.com>
2022-11-16 21:27:03 -0600

    UseItem can now require multiple items
    requires detective badge too now for picking lock

--- Assets/Scripts/ItemScripts/UseItem.cs
+++ Assets/Scripts/ItemScripts/UseItem.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using System.Linq;
 
 public class UseItem : MonoBehaviour
 {
     public string itemName;
-    public int itemId;
+    public string itemId;
     private PlayableDirector director;
 
     public DialogueManager dialogueManager;
@@ -15,9 +16,24 @@ public class UseItem : MonoBehaviour
     private bool itemUsed = false;
     private bool instructionsPresent = false;
 
+    private int id;
+    private List<int> ids = new List<int>();
+    private bool multipleIds = false;
+
     void Awake() 
     {
         director = GetComponent<PlayableDirector>();
+        // change itemId string to list or int
+        if (itemId.Contains(",")) {
+            multipleIds = true;
+            List<string> idsStr = itemId.Split(',').ToList();
+            foreach(string idStr in idsStr) {
+                ids.Add(int.Parse(idStr));
+            }
+        }
+        else {
+            id = int.Parse(itemId);
+        }
     }
 
     // Update is called once per frame
@@ -34,7 +50,23 @@ public class UseItem : MonoBehaviour
 
     void OnTriggerEnter()
     {
-        if (!itemUsed & invManager.Contains(itemId))
+        // check if inventory manager contains all ids listed
+        bool invManContainsAll = false;
+        if (!multipleIds) { // id used, not ids
+            print("one id");
+            if (invManager.Contains(id)) {
+                invManContainsAll = true;
+            }
+        } else { // list of ids used
+            invManContainsAll = true;
+            foreach(int item in ids) {
+                if (!invManager.Contains(item)) {
+                    invManContainsAll = false;
+                }
+            }
+        }
+
+        if (!itemUsed & invManContainsAll)
         {
             dialogueManager.DisplayInstructions("Press E to use " + itemName.ToLower() + ".");
             instructionsPresent = true;
