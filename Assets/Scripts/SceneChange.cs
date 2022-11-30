d03bb2a5bd6a19cabac6ccad56818f22fd835393
Ingrid O'Connor <ingridoco@gmail.com>
2022-11-30 10:36:08 -0600

    added opening scene

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float delay;
+     public string NewLevel= "Bootcamp";
+     void Start()
+     {
+         StartCoroutine(LoadLevelAfterDelay(delay));
+     }
+ 
+     IEnumerator LoadLevelAfterDelay(float delay)
+     {
+         yield return new WaitForSeconds(delay);
+         SceneManager.LoadScene(NewLevel);
+     }
+}
