50082f939f2251316bdb023dd2d9213d16721336
emorrow44 <70652225+emorrow44@users.noreply.github.com>
2022-12-05 23:12:06 -0600

    flashlight sound added

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -6,11 +6,13 @@ public class Flashlight : MonoBehaviour
 {
 
     Light myLight;
+    private AudioSource source;
 
     // Start is called before the first frame update
     void Start()
     {
        myLight = (Light)this.GetComponent("Light");
+       source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,6 +20,7 @@ public class Flashlight : MonoBehaviour
     {
         if (Input.GetKeyDown("f")){
             myLight.enabled = !myLight.enabled;
+            source.Play();
         }
     }
 }
