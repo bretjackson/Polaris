cfb73f86a9c5d26d4a7ce0ee574709a9bb38d64a
Ingrid O'Connor <ingridoco@gmail.com>
2022-12-14 16:31:34 -0600

    fade out music + to black before end scene

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
@@ -9,6 +10,7 @@ public class SceneChange : MonoBehaviour
     public float delay;
     public string NewLevel;
     public bool triggered; // true if triggered, false if timed
+    public GameObject blackOutSquare;
 
     void Start()
     {
@@ -29,7 +31,40 @@ public class SceneChange : MonoBehaviour
  
     IEnumerator LoadLevelAfterDelay(float delay)
     {
+        if (blackOutSquare != null) 
+        {
+            blackOutSquare.SetActive(true);
+            StartCoroutine(FadeToBlack(delay));
+            StartCoroutine(FadeMusic(delay));
+        }
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(NewLevel);
+    }
+
+    IEnumerator FadeToBlack(float delay)
+    {  
+        yield return new WaitForSeconds(delay / 2);
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+        int fadeSpeed = 1;
+        while (objectColor.a < 255) {
+            fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            blackOutSquare.GetComponent<Image>().color = objectColor;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeMusic(float delay)
+    {
+        float elapsedTime = 0;
+        float currentVolume = AudioListener.volume;
+ 
+        while(elapsedTime < delay) {
+            elapsedTime += Time.deltaTime;
+            AudioListener.volume = Mathf.Lerp(currentVolume, 0, elapsedTime / delay);
+            yield return null;
+        }
     }
 }
